c40e15072d5e0b585a429ed89438daeb1f3a6172
mahithachippada <mahithachippada@gmail.com>
2026-02-23 21:19:10 +0530

    Clear repository

--- setup.py
+++
@@ -1,7 +0,0 @@
-"""Setup script for ai-code-reviewer package."""
-from setuptools import setup, find_packages
-
-setup(
-    packages=find_packages(exclude=["tests", "tests.*"]),
-    include_package_data=True,
-)
